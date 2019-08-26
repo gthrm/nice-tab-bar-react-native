49eda00e6edda1f501511a0a0afaf481337d005f
cdROma <mrv24ru@yandex.ru>
2019-08-26 21:43:19 +0300

    draggable card

---
+++ components/BaseConstant.js
@@ -0,0 +1,32 @@
+export const TEST_DATA = [
+    {icon: require('../assets/images/animal1.png'),txt: 1},
+    {icon: require('../assets/images/animal2.png'),txt: 2},
+    {icon: require('../assets/images/animal3.png'),txt: 3},
+    {icon: require('../assets/images/animal4.png'),txt: 4},
+    {icon: require('../assets/images/animal5.png'),txt: 5},
+    {icon: require('../assets/images/animal6.png'),txt: 6},
+    {icon: require('../assets/images/animal7.png'),txt: 7},
+    {icon: require('../assets/images/animal8.png'),txt: 8},
+    {icon: require('../assets/images/animal9.png'),txt: 9},
+    {icon: require('../assets/images/animal10.png'),txt: 10},
+    {icon: require('../assets/images/animal11.png'),txt: 11},
+    {icon: require('../assets/images/animal12.png'),txt: 12},
+    {icon: require('../assets/images/animal13.png'),txt: 13},
+    {icon: require('../assets/images/animal14.png'),txt: 14},
+    {icon: require('../assets/images/animal15.png'),txt: 15},
+    {icon: require('../assets/images/animal16.png'),txt: 16},
+    {icon: require('../assets/images/animal17.png'),txt: 17},
+    {icon: require('../assets/images/animal18.png'),txt: 18},
+]
+
+export const TXT = "Never give up, Never lose hope. \n" +
+    "\n" +
+    "Always have faith, It allows you to cope. \n" +
+    "\n" +
+    "Trying times will pass, As they always do. \n" +
+    "\n" +
+    "Just have patience, Your dreams will come true. \n" +
+    "\n" +
+    "So put on a smile, You'll live through your pain. \n" +
+    "\n" +
+    "Know it will pass, And strength you will gain."
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,138 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Dimensions
+} from 'react-native'
+const { width } = Dimensions.get('window')
+import DragSortableView from '../components/DragSortableView';
+import { TEST_DATA, TXT } from '../components/BaseConstant';
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+const parentWidth = width
+const childrenWidth = width-18
+const childrenHeight = 76
+const marginChildrenTop = 7
+const marginChildrenBottom = 0
+const marginChildrenLeft = 0
+const marginChildrenRight = 7
+
+export default class ScrollPage extends Component {
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      data: TEST_DATA,
+      scrollEnabled: true,
+    }
+  }
+
+  render() {
+    return (
+      <ScrollView
+        ref={(scrollView) => this.scrollView = scrollView}
+        scrollEnabled={this.state.scrollEnabled}
+        style={styles.container}>
+        <Text style={styles.txt}>{TXT}</Text>
+        <View style={styles.sort}>
+          <DragSortableView
+            dataSource={this.state.data}
+            parentWidth={parentWidth}
+
+            childrenWidth={childrenWidth}
+            childrenHeight={childrenHeight}
+
+            marginChildrenTop={marginChildrenTop}
+            marginChildrenBottom={marginChildrenBottom}
+            marginChildrenLeft={marginChildrenLeft}
+            marginChildrenRight={marginChildrenRight}
+
+            onDragStart={(startIndex, endIndex) => {
+              this.setState({
+                scrollEnabled: false
+              })
+            }}
+            onDragEnd={(startIndex) => {
+              this.setState({
+                scrollEnabled: true
+              })
+            }}
+            onDataChange={(data) => {
+              // delete or add data to refresh
+              if (data.length != this.state.data.length) {
+                this.setState({
+                  data: data
+                })
+              }
+            }}
+            onClickItem={(data, item, index) => {
+              // click delete
+              // if (this.state.isEnterEdit) {
+              //     const newData = [...data]
+              //     newData.splice(index,1)
+              //     this.setState({
+              //         data: newData
+              //     })
+              // }
+            }}
+            keyExtractor={(item, index) => item.txt} // FlatList作用一样，优化
+            renderItem={(item, index) => {
+              return this.renderItem(item, index)
+            }}
+          />
+        </View>
+        <Text style={styles.txt}>{TXT}</Text>
+        <Text style={styles.txt}>{TXT}</Text>
+        <Text style={styles.txt}>{TXT}</Text>
+        <Text style={styles.txt}>{TXT}</Text>
+      </ScrollView>
+    )
+  }
+
+  renderItem(item, index) {
+
+    return (
+      <View style={styles.item}>
+        <Image
+          style={styles.item_icon}
+          source={item.icon} />
+        <Text style={styles.txt}>{item.txt}</Text>
+      </View>
+
+    )
+  }
+
 }
 
-SettingsScreen.navigationOptions = {
-  title: 'app.json',
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  txt: {
+    fontSize: 18,
+    lineHeight: 24,
+    padding: 5,
+  },
+  sort: {
+    backgroundColor: "green"
+    // marginLeft: 18
+ 
+  },
+  item: {
+    width: childrenWidth,
+    height: childrenHeight,
+    backgroundColor: '#f0ffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  item_icon: {
+    width: childrenWidth - 4,
+    height: childrenHeight - 4,
+    resizeMode: 'contain',
+    position: 'absolute'
+  }
+})
